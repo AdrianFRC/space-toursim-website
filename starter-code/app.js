cd29944c63d127b90c8c552f042ac26ca8d9eabe
AdrianFRC <adrian.farcasel@gmail.com>
2023-08-14 10:16:30 +0200

    space-toursim-website

---
+++ starter-code/app.js
@@ -0,0 +1,9 @@
+function hideMenu(){
+let navbar = document.getElementById('navbar');
+navbar.style.display = 'none';
+}
+
+function showMenu(){
+    let navbar = document.getElementById('navbar');
+    navbar.style.display = 'block';
+}
